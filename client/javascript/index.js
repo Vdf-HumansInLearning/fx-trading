209523f3e4280ed1e8847c13d50235f8d35f324d
adelazamfirescu <adela.zamfirescu@vodafone.com>
2021-12-21 20:47:15 +0200

    added close functionality for cards, main container, functionality on add widget button, pick widget

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -35,46 +35,7 @@ function createBlotterView() {}
 function createMainWidget() {}
 
 function createPickWidget() {
-  //create column
-  let cardContainer = document.createElement("div");
-  cardContainer.classList.add("col");
-
-  //create card container
-  let card = document.createElement("div");
-  card.className = "card";
-
-  let cardCurrency = document.createElement("div");
-  cardCurrency.className =
-    "card-currency--border px-3 d-flex justify-content-between";
-
-  cardContainer.append(card);
-  card.append(cardCurrency);
-
-  //create title of the card
-  let cardCurrencyDiv = document.createElement("div");
-  cardCurrencyDiv.className = "d-flex align-items-center";
-  let currencyTitle = document.createElement("p");
-  currencyTitle.classList.add("subtitle");
-  let currencyTitleText = document.createTextNode("Pick a currency");
-
-  //create close/delete button for card
-  let closeBtn = document.createElement("button");
-  closeBtn.classList.add("btn-close");
-  closeBtn.setAttribute("type", "button");
-  closeBtn.setAttribute("aria-label", "Close");
-
-  //append elements to card rates div
-  currencyTitle.append(currencyTitleText);
-  cardCurrencyDiv.append(currencyTitle);
-
-  cardCurrency.append(cardCurrencyDiv);
-  cardCurrency.append(closeBtn);
-
-  //create inputs for base and quote currency
-  let cardInputs = document.createElement("div");
-  cardInputs.className = "card-input--center mt-3 px-3";
-
-  //create input groups
+  //input group list
   const cardInputsList = [
     {
       label_for: "inputMainCurrency",
@@ -102,8 +63,46 @@ function createPickWidget() {
     },
   ];
 
-  //create select for currencies
+  //create column
+  let cardContainer = document.createElement("div");
+  cardContainer.classList.add("col");
+  cardContainer.id = "card0";
 
+  //create card container
+  let card = document.createElement("div");
+  card.className = "card";
+
+  //create card currency
+  let cardCurrency = document.createElement("div");
+  cardCurrency.className =
+    "card-currency--border px-3 d-flex justify-content-between";
+
+  //create title of the card
+  let cardCurrencyDiv = document.createElement("div");
+  cardCurrencyDiv.className = "d-flex align-items-center";
+  let currencyTitle = document.createElement("p");
+  currencyTitle.classList.add("subtitle");
+  let currencyTitleText = document.createTextNode("Pick a currency");
+
+  //create close/delete button for card
+  let closeBtn = document.createElement("button");
+  closeBtn.classList.add("btn-close");
+  closeBtn.setAttribute("type", "button");
+  closeBtn.setAttribute("aria-label", "Close");
+  closeBtn.addEventListener("click", () => closeWidget(cardContainer.id));
+
+  //append elements to card rates div
+  currencyTitle.append(currencyTitleText);
+  cardCurrencyDiv.append(currencyTitle);
+
+  cardCurrency.append(cardCurrencyDiv);
+  cardCurrency.append(closeBtn);
+
+  //create inputs for base and quote currency
+  let cardInputs = document.createElement("div");
+  cardInputs.className = "card-input--center mt-3 px-3";
+
+  //create inputs for currencies
   cardInputsList.forEach((cardInput) => {
     let inputGroup = document.createElement("div");
     inputGroup.className = "input-group mb-3";
@@ -113,6 +112,7 @@ function createPickWidget() {
     let labelText = document.createTextNode(cardInput.label_text);
     label.append(labelText);
 
+    //append each input group to parent div
     cardInputs.append(inputGroup);
     inputGroup.append(label);
 
@@ -122,12 +122,12 @@ function createPickWidget() {
     select.setAttribute("id", cardInput.select_id);
 
     //add one disabled option
-    let disabledOption = document.createElement("option");
-    disabledOption.setAttribute("value", "opt_none");
-    let disabledOptionText = document.createTextNode("Choose...");
-    disabledOption.append(disabledOptionText);
+    let defaultOption = document.createElement("option");
+    defaultOption.setAttribute("value", "opt_none");
+    let defaultOptionText = document.createTextNode("Choose...");
+    defaultOption.append(defaultOptionText);
 
-    select.append(disabledOption);
+    select.append(defaultOption);
 
     //iterate through options to append to select
     cardInput.select_options.forEach((selectOption) => {
@@ -150,7 +150,12 @@ function createPickWidget() {
   confirmBtn.setAttribute("type", "button");
   confirmBtnText = document.createTextNode("Ok");
   confirmBtn.append(confirmBtnText);
+  confirmBtn.addEventListener("click", createMainWidget);
   cardActions.append(confirmBtn);
+
+  //add card to cardContainer
+  cardContainer.append(card);
+  card.append(cardCurrency);
 
   //add card rates, card inputs, card actions to card container
   card.append(cardCurrency);
@@ -181,20 +186,104 @@ function createAddWidget() {
   card.append(button);
   cardContainer.append(card);
 
+  cardContainer.addEventListener("click", addNewWidget);
+
   return cardContainer;
 }
+//keep track of how many widgets are on the page
+let widgetsNr = 0;
+function addNewWidget() {
+  //no more that 5 cards
+  if (widgetsNr <= 1) {
+    const newWidget = createPickWidget();
+    cardsRow.append(newWidget);
+    widgetsNr++;
+  } else {
+    generateMessage("You cannot have more than 5 widgets on the page");
+  }
+}
 
-function createRatesView() {}
+function closeWidget(cardId) {
+  document.getElementById(cardId).remove();
+  widgetsNr--;
+}
+
+function generateMessage(message) {
+  //   let toast = document.createElement("div");
+  //   toast.className = "toast-custom success";
+  //   let outerContainer = document.createElement("div");
+  //   outerContainer.className = "outer-container";
+  //   let icon = document.createElement("i");
+  //   outerContainer.className = "fas fa-check-circle";
+  //   outerContainer.append(icon);
+
+  //   let innerContainer = document.createElement("div");
+  //   innerContainer.className = "inner-container";
+  //   let innerContainerTitle = document.createElement("p");
+  //   innerContainerTitle.innerText = "Success";
+  //   let innerContainerMessage = document.createElement("p");
+  //   innerContainerMessage.innerText = message;
+
+  //   innerContainer.append(innerContainerTitle);
+  //   innerContainer.append(innerContainerMessage);
+
+  //   toast.append(outerContainer);
+  //   toast.append(innerContainer);
+
+  let toast = document.createElement("div");
+  toast.className = "tn-box tn-box-color-1";
+  let toastTitle = document.createElement("p");
+  toastTitle.innerText = message;
+
+  toast.append(toastTitle);
+
+  toast.classList.add("tn-box-active");
+
+  mainContainer.append(toast);
+}
+//initialize container for cards
+let cardsRow = null;
+function createRatesView() {
+  console.log("hhhhhhhhhhh");
+  //create main section in which the cards will be
+  let ratesSection = document.createElement("div");
+  ratesSection.className = "col-sm-12 col-md-12 col-lg-6";
+
+  //create title
+  let ratesTitle = document.createElement("h5");
+  ratesTitle.classList.add("color-titles");
+
+  //create cards container
+  let cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("cards-container");
+
+  //create row which will hold cards
+  cardsRow = document.createElement("div");
+  cardsRow.className = "row row-cols-1 row-cols-sm-2 g-4";
+
+  //at first, there will be visible only add button
+  let addWidget = createAddWidget();
+  cardsRow.append(addWidget);
+
+  ratesSection.append(ratesTitle);
+  ratesSection.append(cardsContainer);
+  cardsContainer.append(cardsRow);
+
+  return ratesSection;
+}
 
 function createIndexPage() {
+  //create navbar
   const navBar = createNavigationBar();
   mainContainer.appendChild(navBar);
 
-  const addWidget = createAddWidget();
-  mainContainer.append(addWidget);
+  //create main div
+  let main = document.createElement("main");
+  main.className = "container-fluid row mb-5";
+  mainContainer.append(main);
 
-  const pickWidget = createPickWidget();
-  mainContainer.append(pickWidget);
+  let ratesSection = createRatesView();
+  main.append(ratesSection);
 }
 
 createIndexPage();

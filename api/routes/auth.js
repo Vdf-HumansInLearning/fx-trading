48c7383a428da2dd2a4228829dbb9baa64788d16
alexandrumalinescu <almalinescu@yahoo.com>
2022-01-03 12:33:15 +0200

    login and register validation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,14 +21,14 @@ function validateUser(user) {
   let errArray = [];
 
   if (user.username < 3) {
-    errArray.push("Firstname should have between 3 and 100 characters.");
+    errArray.push("Firstname should have between 3 and 20 characters.");
   }
   if (!user.email) {
     errArray.push("Email is required!");
   }
-  if (checkRegExp(emailRegExp, user.email) === false) {
-    errArray.push("Invalid! Email should contain '@' and a domain!");
-  }
+  // if (checkRegExp(emailRegExp, user.email) === false) {
+  //   errArray.push("Invalid! Email should contain '@' and a domain!");
+  // }
   if (checkRegExp(passRegExp, user.password) === false) {
     errArray.push(
       "Invalid! Password must be 8 characters long and must contain at least: one uppercase, one lowercase, a number and a special character!"
@@ -86,7 +86,6 @@ router.post("/login", function (req, res) {
 // POST NEW REGISTERED USER
 router.post("/register", function (req, res) {
   let users = readFromFile("../db/users.json");
-  console.log(users);
 
   let user = {
     id: users.length + 1,
@@ -95,8 +94,11 @@ router.post("/register", function (req, res) {
     password: req.body.password,
     repassword: req.body.repassword,
   };
+
+  console.log(user);
   let errors = validateUser(user);
   if (errors.length > 0) {
+    // console.log("eroare server");
     res.status(400).send(errors);
     return;
   }

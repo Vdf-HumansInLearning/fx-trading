bc8b816a723a69cd34013f15d66f7d72a4e7cdfd
adelazamfirescu <adela.zamfirescu@vodafone.com>
2022-01-04 18:04:04 +0200

    changed read from file to be on every request

--- api/routes/currencies.js
+++ api/routes/currencies.js
@@ -5,26 +5,33 @@ var router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-//read from file
-let rawdata = fs.readFileSync(path.resolve(__dirname, "../db/currencies.json"));
-let fileContents = JSON.parse(rawdata);
-const currenciesAvailable = fileContents.currencies_available;
-const currenciesRates = fileContents.currency_rates;
-
-//rate = at what rate the user but or sell
 /* GET all currencies available in the app */
 router.get("/currencies", (req, res) => {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../db/currencies.json")
+  );
+  let fileContents = JSON.parse(rawdata);
+  const currenciesAvailable = fileContents.currencies_available;
   res.status(200).json(currenciesAvailable);
 });
 
 /* GET all currency rates with pairings */
 router.get("/currencies/rates", (req, res) => {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../db/currencies.json")
+  );
+  let fileContents = JSON.parse(rawdata);
+  const currenciesRates = fileContents.currency_rates;
   res.status(200).json(currenciesRates);
 });
 
 /* POST one base currency */
 router.post("/currencies", (req, res) => {
-  //get base_cuurency
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../db/currencies.json")
+  );
+  let fileContents = JSON.parse(rawdata);
+  const currenciesRates = fileContents.currency_rates;
   //verify if it is in the list of currencies
   if (req.body.base_currency) {
     let foundCurrency = currenciesRates.find(
@@ -46,6 +53,11 @@ router.post("/currencies", (req, res) => {
 //   quote_currency: "RON"
 // }
 router.post("/currencies/quote", (req, res) => {
+  let rawdata = fs.readFileSync(
+    path.resolve(__dirname, "../db/currencies.json")
+  );
+  let fileContents = JSON.parse(rawdata);
+  const currenciesRates = fileContents.currency_rates;
   if (req.body.base_currency && req.body.quote_currency) {
     let foundBase = currenciesRates.find(
       (pair) => pair.base_currency === req.body.base_currency
